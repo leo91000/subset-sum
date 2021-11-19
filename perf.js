d0b8a19ff2a22e448590f77b6743b5a70c959cd8
Léo Coletta <mail.leo.coletta@gmail.com>
2021-11-19 17:27:44 +0100

    chore: Add perf test

---
+++ perf.js
@@ -0,0 +1,76 @@
+const { find_first_subset_sum } = require('./pkg/subset_sum')
+const { performance } = require('perf_hooks')
+
+function vanillaSubsetSum({
+  array,
+  target,
+  timeout,
+  now = Date.now,
+}) {
+    // if (now() > timeout) {
+    //     throw new Error();
+    // }
+
+    if (array.includes(target)) {
+        return [target];
+    }
+
+    if (array.reduce((a, c) => a + c, 0) === target) {
+        return array;
+    }
+
+    for (let index = 0; index < array.length; index++) {
+        const current = array[index];
+        const withoutCurrent = [...array.slice(0, index), ...array.slice(index + 1)];
+        const newTarget = target - current;
+        const result = vanillaSubsetSum({ array: withoutCurrent, target: newTarget, timeout });
+
+        if (result.length !== 0) {
+            return [current, ...result];
+        }
+    }
+
+    return [];
+}
+
+function wasmSubsetSum({ array, target }) {
+    return find_first_subset_sum(array, target)
+}
+
+function perfTest({ array, target }) {
+    let vanillaResult, wasmResult;
+
+    const vanillaStart = performance.now()
+    try { vanillaResult = vanillaSubsetSum({ array, target }) } catch (e) { vanillaResult = e }
+    const vanillaEnd = performance.now()
+
+    const wasmStart = performance.now()
+    try { wasmResult = wasmSubsetSum({ array, target }) } catch (e) { wasmResult = e }
+    const wasmEnd = performance.now()
+    console.log(JSON.stringify({
+        array,
+        target,
+        vanilla: {
+            duration: vanillaEnd - vanillaStart,
+            result: vanillaResult,
+            sum: vanillaResult.reduce((a, c) => a + c, 0)
+        },
+        wasm: {
+            duration: wasmEnd - wasmStart,
+            result: typeof wasmResult === 'string' ? wasmResult : Array.from(wasmResult),
+            sum: typeof wasmResult === 'string' ? wasmResult : wasmResult.reduce((a, c) => a + c, 0)
+        }
+    }))
+    console.log('-------------------------------')
+}
+
+// Warmup
+vanillaSubsetSum({ array: [1,2,3,4], target: 10 })
+wasmSubsetSum({ array: [1,2,3,4], target: 10 })
+
+// Perf test
+perfTest({ array: [1,2,3,4], target: 10 })
+perfTest({ array: [1,2,3,4], target: 7 })
+perfTest({ array: [1,2,3,4,5,6,7,8,9,10,11], target: 60 })
+perfTest({ array: [1,2,3,4,5,6,7,8,9,10,11], target: -1 })
+perfTest({ array: [1,2,3,4,5,6,7,8,9,10,11,-1], target: 0 })
--- src/subset_sum.rs
+++ src/subset_sum.rs
@@ -4,33 +4,29 @@ pub enum SubsetSumError {
 }
 
 pub fn find_first_subset_sum(list: Vec<i32>, sum: i32) -> Result<Vec<i32>, SubsetSumError> {
-    {
-        let mut current_sum = 0;
-        for (index, &current) in list.iter().enumerate() {
-            if current == sum {
-                return Ok(vec![current])
+    match list.iter().find(|&&v| v == sum) {
+        Some(&value) => return Ok(vec![value]),
+        None => {
+            let total: i32 = list.iter().sum();
+            if total == sum {
+                return Ok(list);
             }
-            current_sum += current;
-            if sum == current_sum {
-                let mut result = list.clone();
-                result.truncate(index + 1);
-                return Ok(result)
+
+            for (index, &current) in list.iter().enumerate() {
+                let mut subset = list.clone();
+                subset.swap_remove(index);
+                match find_first_subset_sum(subset, sum - current) {
+                    Ok(mut result) => {
+                        result.push(current);
+                        return Ok(result)
+                    }
+                    _ => {
+                        continue;
+                    }
+                }
             }
         }
     }
 
-    for (index, &current) in list.iter().enumerate() {
-        let mut subset = list.clone();
-        subset.swap_remove(index);
-        match find_first_subset_sum(subset, sum - current) {
-            Ok(mut result) => {
-                result.push(current);
-                return Ok(result)
-            }
-            _ => {
-                continue;
-            }
-        }
-    }
     Err(SubsetSumError::NoMatch)
 }
