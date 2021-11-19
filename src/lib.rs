48c9d7bbf74b8d7daaa361107b89b71aab56bb75
Léo Coletta <mail.leo.coletta@gmail.com>
2021-11-19 18:53:20 +0100

    chore: Add perf test

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,6 @@ mod subset_sum;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-// #[cfg(feature = "wee_alloc")]
-// #[global_allocator]
-// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 pub fn find_first_subset_sum(list: Vec<i32>, sum: i32) -> Result<Vec<i32>, JsValue> {
     match subset_sum::find_first_subset_sum(list, sum) {
